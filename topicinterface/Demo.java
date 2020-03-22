b6cb703cda3748d288087cf13a22cacb946ef246
sriramperumalla <sriram83.tech@gmail.com>
2020-03-22 18:05:05 +0530

    Creating devops pipeline files

--- topicinterface/Demo.java
+++ topicinterface/Demo.java
@@ -1,7 +1,7 @@
 package topicinterface;
 
 public class Demo{
-    public static void main(String[] args) {
+    public static void main(s[] args) {
         Bird b=new Bird();
         b.eat();
         b.fly();
