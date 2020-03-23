ca5d310f677f7cf9cc410daad3986aec52c11f01
sriramperumalla <sriram83.tech@gmail.com>
2020-03-23 15:32:25 +0530

    move to abstractclass

---
+++ topicabstract/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+package topicabstract;
+public class AddTwoNumbers{
+    public static void main(String[] args)
+    {
+        int num1=4, num2=6, sum;
+        sum=num1+num2;
+        System.out.println("sum of these two numbers:"+sum);
+    }
+}
