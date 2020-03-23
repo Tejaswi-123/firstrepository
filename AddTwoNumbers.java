8380e1593e8839879b0adafb21dd07fce47f3e19
sriramperumalla <sriram83.tech@gmail.com>
2020-03-23 15:34:15 +0530

    qqq

--- AddTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-public class AddTwoNumbers{
-    public static void main(String[] args)
-    {
-        int num1=4, num2=6, sum;
-        sum=num1+num2;
-        System.out.println("sum of these two numbers:"+sum);
-    }
-}
