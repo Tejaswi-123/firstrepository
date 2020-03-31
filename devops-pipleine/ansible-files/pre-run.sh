46b9ec8c60df69c7e9aa5427ec1a8489a6efabc7
sriramperumalla <sriram83.tech@gmail.com>
2020-03-31 10:12:24 +0530

    modified cp commmand to shell script

--- devops-pipleine/ansible-files/pre-run.sh
+++ devops-pipleine/ansible-files/pre-run.sh
@@ -1,4 +1,4 @@
 export WORKSPACE="/var/lib/jenkins/workspace/seedjob"
-cp -r "$WORKSPACE/devops-ansible/devops-pipleine/ansible-files/*" /etc/ansible/ 
+cp -r $WORKSPACE/devops-ansible/devops-pipleine/ansible-files/* /etc/ansible/ 
 cd /etc/ansible/
 /usr/bin/ansible-playbook nginx-deploy.yaml --vault-password-file /etc/ansible/vault_password
