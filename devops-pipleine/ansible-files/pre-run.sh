8920a4dc91795bd3f96ac992caf45da6420319e8
sriramperumalla <sriram83.tech@gmail.com>
2020-04-01 10:35:08 +0530

    added ANSIBLE_LOG_PATH to all jobs scripts

--- devops-pipleine/ansible-files/pre-run.sh
+++ devops-pipleine/ansible-files/pre-run.sh
@@ -1,5 +1,5 @@
 export WORKSPACE="/var/lib/jenkins/workspace/seedjob"
-
+export ANSIBLE_LOG_PATH="/var/lib/jenkins/workspace/seedjob/ansible.log"
 cp -r $WORKSPACE/devops-ansible/devops-pipleine/ansible-files/* /etc/ansible/ 
 cd /etc/ansible/
 /usr/bin/ansible-playbook nginx-deploy.yaml --vault-password-file /etc/ansible/vault_password
