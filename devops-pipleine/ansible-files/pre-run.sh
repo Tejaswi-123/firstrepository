7c9054fe66c0f7b1b1716b84df913906bf48ff8e
sriramperumalla <sriram83.tech@gmail.com>
2020-04-01 07:57:32 +0530

    change the ansible code

--- devops-pipleine/ansible-files/pre-run.sh
+++ devops-pipleine/ansible-files/pre-run.sh
@@ -1,5 +1,5 @@
-export WORKSPACE="/var/lib/jenkins/workspace/samplejob"
+export WORKSPACE="/var/lib/jenkins/workspace/seedjob"
 
 cp -r $WORKSPACE/devops-ansible/devops-pipleine/ansible-files/* /etc/ansible/ 
 cd /etc/ansible/
-/usr/bin/ansible-playbook apache.yaml --vault-password-file /etc/ansible/vault_password
+/usr/bin/ansible-playbook nginx-deploy.yaml --vault-password-file /etc/ansible/vault_password
